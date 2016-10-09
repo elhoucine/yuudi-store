c11a6f9f96622d484564265b4c3b4462636ba6f4
Mohammed Mazil <mohamed.mazil@gmail.com>
2016-10-09 15:56:55 +0100

    change packs imgaes

--- imports/startup/server/packs.js
+++ imports/startup/server/packs.js
@@ -3,6 +3,6 @@ import Packs from '../../api/packs/packs.js';
 if (Packs.find().count() < 1) {
   Packs.insert({published: true, name:"Pack", details: "Lid est laborum dolo rumes fugats untras.", photoBig: "/images/pack.jpg", photoSmall: "banner_01.jpg", price:10, promotion:8.99});
   Packs.insert({published: true, name:"Pack", details: "Lid est laborum dolo rumes fugats untras.", photoBig: "/images/pack.jpg", photoSmall: "banner_01.jpg", price:10, promotion:8.99});
-  Packs.insert({published: true, name:"Pack", details: "Lid est laborum dolo rumes fugats untras.", photoBig: "/images/pack.jpg", photoSmall: "banner_01.jpg", price:10, promotion:8.99});
+  Packs.insert({published: true, name:"Pack", details: "Lid est laborum dolo rumes fugats untras.", photoBig: "/images/pack.jpg", photoSmall: "banner_03.jpg", price:10, promotion:8.99});
   Packs.insert({published: true, name:"Pack", details: "Lid est laborum dolo rumes fugats untras.", photoBig: "/images/pack.jpg", photoSmall: "banner_01.jpg", price:10, promotion:8.99});
 }
