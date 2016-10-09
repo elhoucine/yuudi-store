ff603ec5b3e5a589a52a04dfa76403dfda59379a
Elhoucine <houcineparade@gmail.com>
2016-10-09 15:37:28 +0100

    superfish

--- imports/ui/components/header.js
+++ imports/ui/components/header.js
@@ -1,3 +1,13 @@
+import superfish from 'superfish';
+
 Template.header.rendered = function() {
-  console.log(jQuery('ul.sf-menu').superfish());
+  $('ul.sf-menu').superfish({
+    delay:       300,
+    animation:   {opacity:'show'},
+    animationOut: {opacity: 'hide'},
+    speed:       'fast',
+    disableHI:   false,
+    cssArrows:   true,
+    autoArrows:  true
+  });
 }
