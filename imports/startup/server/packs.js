74eb87d1c7a2c636c1a728f4efd98a2b11dfc59b
Mohammed Mazil <mohamed.mazil@gmail.com>
2016-10-09 15:57:35 +0100

    change packs names

--- imports/startup/server/packs.js
+++ imports/startup/server/packs.js
@@ -1,8 +1,8 @@
 import Packs from '../../api/packs/packs.js';
 
 if (Packs.find().count() < 1) {
-  Packs.insert({published: true, name:"Pack", details: "Lid est laborum dolo rumes fugats untras.", photoBig: "/images/pack.jpg", photoSmall: "banner_01.jpg", price:10, promotion:8.99});
-  Packs.insert({published: true, name:"Pack", details: "Lid est laborum dolo rumes fugats untras.", photoBig: "/images/pack.jpg", photoSmall: "banner_01.jpg", price:10, promotion:8.99});
-  Packs.insert({published: true, name:"Pack", details: "Lid est laborum dolo rumes fugats untras.", photoBig: "/images/pack.jpg", photoSmall: "banner_03.jpg", price:10, promotion:8.99});
-  Packs.insert({published: true, name:"Pack", details: "Lid est laborum dolo rumes fugats untras.", photoBig: "/images/pack.jpg", photoSmall: "banner_01.jpg", price:10, promotion:8.99});
+  Packs.insert({published: true, name:"Pack Basic", details: "Lid est laborum dolo rumes fugats untras.", photoBig: "/images/pack.jpg", photoSmall: "banner_01.jpg", price:10, promotion:8.99});
+  Packs.insert({published: true, name:"Pack Extra", details: "Lid est laborum dolo rumes fugats untras.", photoBig: "/images/pack.jpg", photoSmall: "banner_01.jpg", price:10, promotion:8.99});
+  Packs.insert({published: true, name:"Pack Toilette", details: "Lid est laborum dolo rumes fugats untras.", photoBig: "/images/pack.jpg", photoSmall: "banner_03.jpg", price:10, promotion:8.99});
+  Packs.insert({published: true, name:"Pack Personalisé", details: "Lid est laborum dolo rumes fugats untras.", photoBig: "/images/pack.jpg", photoSmall: "banner_01.jpg", price:10, promotion:8.99});
 }
