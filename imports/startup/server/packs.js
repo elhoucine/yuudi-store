dcf24d9c910cab3a9072f4d567c2fee28c6f2c4a
Mohammed Mazil <mohamed.mazil@gmail.com>
2016-10-09 15:55:51 +0100

    change packs imgaes

--- imports/startup/server/packs.js
+++ imports/startup/server/packs.js
@@ -1,8 +1,8 @@
 import Packs from '../../api/packs/packs.js';
 
 if (Packs.find().count() < 1) {
-  Packs.insert({published: true, name:"Pack", details: "lorem", photoBig: "/images/pack.jpg", photoSmall: "/images/pack.jpg", price:10, promotion:8.99});
-  Packs.insert({published: true, name:"Pack", details: "lorem", photoBig: "/images/pack.jpg", photoSmall: "/images/pack.jpg", price:10, promotion:8.99});
-  Packs.insert({published: true, name:"Pack", details: "lorem", photoBig: "/images/pack.jpg", photoSmall: "/images/pack.jpg", price:10, promotion:8.99});
-  Packs.insert({published: true, name:"Pack", details: "lorem", photoBig: "/images/pack.jpg", photoSmall: "/images/pack.jpg", price:10, promotion:8.99});
+  Packs.insert({published: true, name:"Pack", details: "Lid est laborum dolo rumes fugats untras.", photoBig: "/images/pack.jpg", photoSmall: "banner_01.jpg", price:10, promotion:8.99});
+  Packs.insert({published: true, name:"Pack", details: "Lid est laborum dolo rumes fugats untras.", photoBig: "/images/pack.jpg", photoSmall: "banner_01.jpg", price:10, promotion:8.99});
+  Packs.insert({published: true, name:"Pack", details: "Lid est laborum dolo rumes fugats untras.", photoBig: "/images/pack.jpg", photoSmall: "banner_01.jpg", price:10, promotion:8.99});
+  Packs.insert({published: true, name:"Pack", details: "Lid est laborum dolo rumes fugats untras.", photoBig: "/images/pack.jpg", photoSmall: "banner_01.jpg", price:10, promotion:8.99});
 }
