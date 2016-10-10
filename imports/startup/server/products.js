0d87452098ecc8b87fa413b1b6245338054a95aa
Mohammed Mazil <mohamed.mazil@gmail.com>
2016-10-10 11:41:17 +0100

    add detail to products

--- imports/startup/server/products.js
+++ imports/startup/server/products.js
@@ -1,27 +1,23 @@
 import Products from '../../api/products/products.js';
 
 if( Products.find().count() < 1){
-  Products.insert({published: true, name:"Product", details: "lorem", photoBig: "/images/produit.jpg", photoSmall: "/images/produit.jpg", price:10, promotion:8.99});
-  Products.insert({published: true, name:"Product", details: "lorem", photoBig: "/images/produit1.jpg", photoSmall: "/images/produit1.jpg", price:10, promotion:8.99});
-  Products.insert({published: true, name:"Product", details: "lorem", photoBig: "/images/produit2.jpg", photoSmall: "/images/produit2.jpg", price:10, promotion:8.99});
-  Products.insert({published: true, name:"Product", details: "lorem", photoBig: "/images/produit3.jpg", photoSmall: "/images/produit3.jpg", price:10, promotion:8.99});
-  Products.insert({published: true, name:"Product", details: "lorem", photoBig: "/images/produit4.jpg", photoSmall: "/images/produit4.jpg", price:10, promotion:8.99});
+  Products.insert({published: true, name:"Lait UHT 1L", details: "lorem", photoBig: "/images/produit.jpg", photoSmall: "/images/produit.jpg", price:10, promotion:8.99});
+  Products.insert({published: true, name:"La Vache Qui Rit", details: "lorem", photoBig: "/images/produit1.jpg", photoSmall: "/images/produit1.jpg", price:10, promotion:8.99});
+  Products.insert({published: true, name:"Thé Sultan", details: "lorem", photoBig: "/images/produit2.jpg", photoSmall: "/images/produit2.jpg", price:10, promotion:8.99});
+  Products.insert({published: true, name:"Isabel Thon", details: "lorem", photoBig: "/images/produit3.jpg", photoSmall: "/images/produit3.jpg", price:10, promotion:8.99});
 
-  Products.insert({published: true, name:"Product", details: "lorem", photoBig: "/images/produit.jpg", photoSmall: "/images/produit.jpg", price:10, promotion:8.99});
-  Products.insert({published: true, name:"Product", details: "lorem", photoBig: "/images/produit1.jpg", photoSmall: "/images/produit1.jpg", price:10, promotion:8.99});
-  Products.insert({published: true, name:"Product", details: "lorem", photoBig: "/images/produit2.jpg", photoSmall: "/images/produit2.jpg", price:10, promotion:8.99});
-  Products.insert({published: true, name:"Product", details: "lorem", photoBig: "/images/produit3.jpg", photoSmall: "/images/produit3.jpg", price:10, promotion:8.99});
-  Products.insert({published: true, name:"Product", details: "lorem", photoBig: "/images/produit4.jpg", photoSmall: "/images/produit4.jpg", price:10, promotion:8.99});
+  Products.insert({published: true, name:"Lait UHT 1L", details: "lorem", photoBig: "/images/produit.jpg", photoSmall: "/images/produit.jpg", price:10, promotion:8.99});
+  Products.insert({published: true, name:"La Vache Qui Rit", details: "lorem", photoBig: "/images/produit1.jpg", photoSmall: "/images/produit1.jpg", price:10, promotion:8.99});
+  Products.insert({published: true, name:"Thé Sultan", details: "lorem", photoBig: "/images/produit2.jpg", photoSmall: "/images/produit2.jpg", price:10, promotion:8.99});
+  Products.insert({published: true, name:"Isabel Thon", details: "lorem", photoBig: "/images/produit3.jpg", photoSmall: "/images/produit3.jpg", price:10, promotion:8.99});
 
-  Products.insert({published: true, name:"Product", details: "lorem", photoBig: "/images/produit.jpg", photoSmall: "/images/produit.jpg", price:10, promotion:8.99});
-  Products.insert({published: true, name:"Product", details: "lorem", photoBig: "/images/produit1.jpg", photoSmall: "/images/produit1.jpg", price:10, promotion:8.99});
-  Products.insert({published: true, name:"Product", details: "lorem", photoBig: "/images/produit2.jpg", photoSmall: "/images/produit2.jpg", price:10, promotion:8.99});
+  Products.insert({published: true, name:"Lait UHT 1L", details: "lorem", photoBig: "/images/produit.jpg", photoSmall: "/images/produit.jpg", price:10, promotion:8.99});
+  Products.insert({published: true, name:"La Vache Qui Rit", details: "lorem", photoBig: "/images/produit1.jpg", photoSmall: "/images/produit1.jpg", price:10, promotion:8.99});
+  Products.insert({published: true, name:"Thé Sultan", details: "lorem", photoBig: "/images/produit2.jpg", photoSmall: "/images/produit2.jpg", price:10, promotion:8.99});
   Products.insert({published: true, name:"Product", details: "lorem", photoBig: "/images/produit3.jpg", photoSmall: "/images/produit3.jpg", price:10, promotion:8.99});
-  Products.insert({published: true, name:"Product", details: "lorem", photoBig: "/images/produit4.jpg", photoSmall: "/images/produit4.jpg", price:10, promotion:8.99});
 
-  Products.insert({published: true, name:"Product", details: "lorem", photoBig: "/images/produit.jpg", photoSmall: "/images/produit.jpg", price:10, promotion:8.99});
-  Products.insert({published: true, name:"Product", details: "lorem", photoBig: "/images/produit1.jpg", photoSmall: "/images/produit1.jpg", price:10, promotion:8.99});
-  Products.insert({published: true, name:"Product", details: "lorem", photoBig: "/images/produit2.jpg", photoSmall: "/images/produit2.jpg", price:10, promotion:8.99});
-  Products.insert({published: true, name:"Product", details: "lorem", photoBig: "/images/produit3.jpg", photoSmall: "/images/produit3.jpg", price:10, promotion:8.99});
-  Products.insert({published: true, name:"Product", details: "lorem", photoBig: "/images/produit4.jpg", photoSmall: "/images/produit4.jpg", price:10, promotion:8.99});
+  Products.insert({published: true, name:"Lait UHT 1L", details: "lorem", photoBig: "/images/produit.jpg", photoSmall: "/images/produit.jpg", price:10, promotion:8.99});
+  Products.insert({published: true, name:"La Vache Qui Rit", details: "lorem", photoBig: "/images/produit1.jpg", photoSmall: "/images/produit1.jpg", price:10, promotion:8.99});
+  Products.insert({published: true, name:"Thé Sultan", details: "lorem", photoBig: "/images/produit2.jpg", photoSmall: "/images/produit2.jpg", price:10, promotion:8.99});
+  Products.insert({published: true, name:"Isabel Thon", details: "lorem", photoBig: "/images/produit3.jpg", photoSmall: "/images/produit3.jpg", price:10, promotion:8.99});
 }
--- imports/ui/components/detail/detail.js
+++ imports/ui/components/detail/detail.js
@@ -1,4 +1,5 @@
 import Packs from '/imports/api/packs/packs.js';
+import Products from '/imports/api/products/products.js';
 import './detail.html';
 
 Template.detail.rendered=function () {
@@ -8,6 +9,11 @@ Template.detail.rendered=function () {
 Template.detail.helpers({
   detail() {
     var id = FlowRouter.getParam("id");
-    return Packs.findOne({"_id":id});
+    var query = FlowRouter.getQueryParam("detail");
+    if(query == "pack"){
+        return Packs.findOne({"_id":id});
+    }else if(query == "produit"){
+        return Products.findOne({"_id":id});
+    }
   }
 })
