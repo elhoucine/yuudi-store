0e7501e47f627038e322a1cede620cfe98f9f584
Elhoucine <houcineparade@gmail.com>
2016-10-24 23:40:19 +0100

    step two

--- imports/ui/components/checkout/checkout.js
+++ imports/ui/components/checkout/checkout.js
@@ -1,9 +1,18 @@
 Template.checkout.rendered = function() {
-
+  $("#step1 a").toggleClass("inStep");
+  $("#step2 a").toggleClass("inStep");
 }
 
 Template.checkout.helpers({
-
+  stepOne() {
+    return false;
+  },
+  stepTwo() {
+    return true;
+  },
+  stepThree() {
+    return false;
+  }
 });
 
 Template.checkout.events({
