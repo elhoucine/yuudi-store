e2bbc77331cecf0248f71ee582254be2664bcffa
Elhoucine <houcineparade@gmail.com>
2016-10-29 13:37:45 +0100

    Notify by SMS

--- imports/api/orders/server/methods.js
+++ imports/api/orders/server/methods.js
@@ -1,3 +1,5 @@
+import { check } from 'meteor/check';
+import { HTTP } from 'meteor/http';
 import Carts from '/imports/api/carts/carts.js';
 import Orders from '../orders.js';
 
@@ -22,10 +24,30 @@ Meteor.methods({
     const userCartItems = userCart[0].items;
 
     //Add new order
-    return Orders.insert({
+    const newOrder = Orders.insert({
         userId: this.userId,
         status: 'created',
         items: userCartItems
       });
+
+    // Notify
+    const phoneNumber = Meteor.user().profile.phoneNumber;
+    const fullName = Meteor.user().profile.fullName;
+    Meteor.call("notifyBySms", fullName, phoneNumber);
+
+    return newOrder;
+  },
+  notifyBySms(fullName, phoneNumber) {
+    check(fullName, String);
+    check(phoneNumber, String);
+    this.unblock();
+
+    try {
+      var result = HTTP.call("GET", `https://rest.nexmo.com/sms/json?api_key=56d91d40&api_secret=43cd95c9ecb009bd&from=YUUDI&to=212679499888&text=New+Order+Placed+${fullName}+${phoneNumber}`);
+      return true;
+    } catch (e) {
+      // Got a network error, time-out or HTTP error in the 400 or 500 range.
+      throw new Meteor.Error('SMS Failed'+e);
+    }
   }
 });
