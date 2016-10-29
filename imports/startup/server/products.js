a304461873593c2352cd4a4edafa60c67f8c6bb4
Mohammed Mazil <mohamed.mazil@gmail.com>
2016-10-29 13:18:17 +0100

    fruits

--- imports/startup/server/products.js
+++ imports/startup/server/products.js
@@ -4,148 +4,46 @@ if( Products.find().count() < 1){
 
   Products.insert({
       published: true,
-      name:"Lait UHT 1L",
+      name:"Banane",
       details: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      photoCover: "/images/produit.jpg",
-      photos:["/images/produit.jpg","/images/produit1.jpg","/images/produit2.jpg","/images/produit3.jpg"],
+      photoCover: "/images/banan.jpg",
+      photos:["/images/banan.jpg"],
       price:10,
       promotion:8.99});
 
   Products.insert({
       published: true,
-      name:"La Vache Qui Rit",
+      name:"Orange",
       details: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      photoCover: "/images/produit1.jpg",
-      photos:["/images/produit.jpg","/images/produit1.jpg","/images/produit2.jpg","/images/produit3.jpg"],
+      photoCover: "/images/orange.jpg",
+      photos:["/images/orange.jpg"],
       price:8,
       promotion:8.99});
 
   Products.insert({
       published: true,
-      name:"Thé Sultan",
+      name:"Pomme",
       details: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      photoCover: "/images/produit2.jpg",
-      photos:["/images/produit.jpg","/images/produit1.jpg","/images/produit2.jpg","/images/produit3.jpg"],
+      photoCover: "/images/apple.jpg",
+      photos:["/images/apple.jpg"],
       price:7,
       promotion:8.99});
 
   Products.insert({
       published: true,
-      name:"Isabel Thon",
+      name:"Grain de raisin (noir)",
       details: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      photoCover: "/images/produit3.jpg",
-      photos:["/images/produit.jpg","/images/produit1.jpg","/images/produit2.jpg","/images/produit3.jpg"],
-      price:18,
-      promotion:8.99});
-
-
-  Products.insert({
-      published: true,
-      name:"Lait UHT 1L",
-      details: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      photoCover: "/images/produit.jpg",
-      photos:["/images/produit.jpg","/images/produit1.jpg","/images/produit2.jpg","/images/produit3.jpg"],
-      price:10,
-      promotion:8.99});
-
-  Products.insert({
-      published: true,
-      name:"La Vache Qui Rit",
-      details: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      photoCover: "/images/produit1.jpg",
-      photos:["/images/produit.jpg","/images/produit1.jpg","/images/produit2.jpg","/images/produit3.jpg"],
-      price:8,
-      promotion:8.99});
-
-  Products.insert({
-      published: true,
-      name:"Thé Sultan",
-      details: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      photoCover: "/images/produit2.jpg",
-      photos:["/images/produit.jpg","/images/produit1.jpg","/images/produit2.jpg","/images/produit3.jpg"],
+      photoCover: "/images/black_grape.jpg",
+      photos:["/images/black_grape.jpg"],
       price:7,
       promotion:8.99});
 
   Products.insert({
       published: true,
-      name:"Isabel Thon",
+      name:"Grain de raisin (vert)",
       details: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      photoCover: "/images/produit3.jpg",
-      photos:["/images/produit.jpg","/images/produit1.jpg","/images/produit2.jpg","/images/produit3.jpg"],
-      price:18,
-      promotion:8.99});
-
-
-  Products.insert({
-      published: true,
-      name:"Lait UHT 1L",
-      details: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      photoCover: "/images/produit.jpg",
-      photos:["/images/produit.jpg","/images/produit1.jpg","/images/produit2.jpg","/images/produit3.jpg"],
-      price:10,
-      promotion:8.99});
-
-  Products.insert({
-      published: true,
-      name:"La Vache Qui Rit",
-      details: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      photoCover: "/images/produit1.jpg",
-      photos:["/images/produit.jpg","/images/produit1.jpg","/images/produit2.jpg","/images/produit3.jpg"],
-      price:8,
-      promotion:8.99});
-
-  Products.insert({
-      published: true,
-      name:"Thé Sultan",
-      details: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      photoCover: "/images/produit2.jpg",
-      photos:["/images/produit.jpg","/images/produit1.jpg","/images/produit2.jpg","/images/produit3.jpg"],
+      photoCover: "/images/green_grape.jpg",
+      photos:["/images/green_grape.jpg"],
       price:7,
-      promotion:8.99});
-
-  Products.insert({
-      published: true,
-      name:"Isabel Thon",
-      details: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      photoCover: "/images/produit3.jpg",
-      photos:["/images/produit.jpg","/images/produit1.jpg","/images/produit2.jpg","/images/produit3.jpg"],
-      price:18,
-      promotion:8.99});
-
-
-  Products.insert({
-      published: true,
-      name:"Lait UHT 1L",
-      details: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      photoCover: "/images/produit.jpg",
-      photos:["/images/produit.jpg","/images/produit1.jpg","/images/produit2.jpg","/images/produit3.jpg"],
-      price:10,
-      promotion:8.99});
-
-  Products.insert({
-      published: true,
-      name:"La Vache Qui Rit",
-      details: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      photoCover: "/images/produit1.jpg",
-      photos:["/images/produit.jpg","/images/produit1.jpg","/images/produit2.jpg","/images/produit3.jpg"],
-      price:8,
-      promotion:8.99});
-
-  Products.insert({
-      published: true,
-      name:"Thé Sultan",
-      details: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      photoCover: "/images/produit2.jpg",
-      photos:["/images/produit.jpg","/images/produit1.jpg","/images/produit2.jpg","/images/produit3.jpg"],
-      price:7,
-      promotion:8.99});
-
-  Products.insert({
-      published: true,
-      name:"Isabel Thon",
-      details: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      photoCover: "/images/produit3.jpg",
-      photos:["/images/produit.jpg","/images/produit1.jpg","/images/produit2.jpg","/images/produit3.jpg"],
-      price:18,
       promotion:8.99});
 }
