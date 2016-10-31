2aa656e45923f98dd2f716e29e310aaefe890e23
Elhoucine <houcineparade@gmail.com>
2016-10-31 11:12:24 +0000

    Publish orders for admin

--- imports/api/orders/server/publications.js
+++ imports/api/orders/server/publications.js
@@ -3,3 +3,10 @@ import Orders from '../orders.js';
 Meteor.publish('allMyOrders', function() {
   return Orders.find({'userId': this.userId});
 });
+
+Meteor.publish('allOrders', function() {
+  if(Roles.userInRole(this.userId, "admin")){
+    return Orders.find({});
+  }
+  this.Ready();
+});
